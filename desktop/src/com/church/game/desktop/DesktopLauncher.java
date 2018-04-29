4442ff7ab6e130284c97f62a93d0a62e303202af
JavaCafe01 <JavaCafe01@users.noreply.github.com>
2018-04-29 12:04:36 -0700

    Added new empty project

---
+++ desktop/src/com/church/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.church.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.church.game.TankGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TankGame(), config);
+	}
+}
