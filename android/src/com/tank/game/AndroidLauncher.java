075a222e5bb97dacb02da0a8dac804c8e6ba7814
JavaCafe01 <JavaCafe01@users.noreply.github.com>
2018-05-22 16:51:49 -0700

    Added android project

---
+++ android/src/com/tank/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.tank.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.tank.game.TankInfinity;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new TankInfinity(), config);
+	}
+}
