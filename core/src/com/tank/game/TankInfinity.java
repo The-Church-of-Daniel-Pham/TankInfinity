2f8f6cf0715bb02e61f8c07f505fd7adf03247a2
Android <Android@00-67077.pusd.dom>
2018-05-22 11:22:52 -0700

    Restart!

--- core/src/com/tank/game/TankInfinity.java
+++ core/src/com/tank/game/TankInfinity.java
@@ -1,4 +1,4 @@
-package com.church.game;
+package com.tank.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
@@ -6,7 +6,7 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class TankGame extends ApplicationAdapter {
+public class TankInfinity extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
 	
