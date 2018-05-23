fc9c107790b19c58d97d33e55a5e1eebdbab8e59
SamuelHunter <38480476+SamuelHunter@users.noreply.github.com>
2018-05-23 12:59:46 -0700

    added app icon

--- desktop/src/com/ttr/game/desktop/DesktopLauncher.java
+++ desktop/src/com/ttr/game/desktop/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package com.ttr.game.desktop;
 
+import com.badlogic.gdx.Files.FileType;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.ttr.TankTankRevolution;
@@ -11,6 +12,7 @@ public class DesktopLauncher {
 	public static void main(String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration(); // new config
 		config.title = "Tank Tank Revolution";
+		config.addIcon("menu/dpham_32.png", FileType.Internal);	// app icon; cannot put in assets since this class is created earlier
 		config.resizable = false;	// cannot resize window manually
 		config.vSyncEnabled = false; // vertical sync is true
 		config.foregroundFPS = 0; // setting to 0 disables foreground fps throttling
