ad0667e68d2336ce04ba0246186973ea9cbbf8e8
SamuelHunter <38480476+SamuelHunter@users.noreply.github.com>
2018-05-24 12:09:46 -0700

    stage classes

---
+++ core/src/com/tank/stage/SettingsMenu.java
@@ -0,0 +1,12 @@
+package com.tank.stage;
+
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+
+public class SettingsMenu extends Stage {
+	protected Table uiTable;
+
+	public SettingsMenu() {
+		
+	}
+}
