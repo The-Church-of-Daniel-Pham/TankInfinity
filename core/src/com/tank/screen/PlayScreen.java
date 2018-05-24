0bb960b149627a1e97814bb112a95811a9f92485
SamuelHunter <38480476+SamuelHunter@users.noreply.github.com>
2018-05-24 12:22:40 -0700

    screen classes

---
+++ core/src/com/tank/screen/PlayScreen.java
@@ -0,0 +1,58 @@
+package com.tank.screen;
+
+import com.badlogic.gdx.Screen;
+import com.tank.game.TankInfinity;
+import com.tank.stage.Level;
+import com.tank.stage.LevelHUD;
+import com.tank.stage.PauseMenu;
+
+public class PlayScreen implements Screen {
+	protected TankInfinity game;
+	public Level level;
+	public LevelHUD levelhud;
+	public PauseMenu pauseMenu;
+	protected boolean paused;
+
+	public PlayScreen(TankInfinity game) {
+		
+	}
+	
+	@Override
+	public void show() {
+		
+	}
+
+	@Override
+	public void hide() {
+		
+	}
+	
+    @Override
+	public void resize(int width, int height) {
+		
+	}
+	
+    @Override
+    public void render(float delta) {
+    	
+    }
+    
+	public void exitButton() {
+		
+	}
+
+	@Override
+	public void dispose() {
+		
+	}
+	
+	@Override
+	public void pause() {
+		
+	}
+
+	@Override
+	public void resume() {
+		
+	}
+}
---
+++ core/src/com/tank/screen/SettingsMenuScreen.java
@@ -0,0 +1,54 @@
+package com.tank.screen;
+
+import com.badlogic.gdx.Screen;
+import com.tank.game.TankInfinity;
+import com.tank.stage.SettingsMenu;
+
+public class SettingsMenuScreen implements Screen {
+	public SettingsMenu settingsMenu;
+	private TankInfinity game;
+	
+	public SettingsMenuScreen (TankInfinity game) {
+		
+	}
+	
+	@Override
+	public void show() {
+		
+	}
+	
+	@Override
+	public void hide() {
+		
+	}
+	
+	@Override
+	public void resize (int width, int height) {
+		
+	}
+	
+	@Override
+	public void render (float delta) {
+		
+	}
+
+	public void dispose() {
+		
+	}
+	
+	public void exitButton() {
+		
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+		
+	}
+}
