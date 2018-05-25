5591ecbfe912e8d092cc8db6af64f413134bbf1f
SamuelHunter <38480476+SamuelHunter@users.noreply.github.com>
2018-05-25 11:47:40 -0700

    added assets, constants, desktoplauncher, game, loadingscreen, and loading

--- core/src/com/tank/screen/LoadingScreen.java
+++ core/src/com/tank/screen/LoadingScreen.java
@@ -1,34 +1,62 @@
 package com.tank.screen;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
 import com.tank.game.TankInfinity;
 import com.tank.stage.Loading;
+import com.tank.utils.Assets;
 
 public class LoadingScreen implements Screen {
 	public Loading loading;
 	protected TankInfinity game;
 
 	public LoadingScreen(TankInfinity game) {
-		
+		this.game = game;
+		// starts loading everything, but not waiting to continue
+		Assets.loadAll();
+		// wait until loading stage's assets are loaded
+		Assets.manager.finishLoadingAsset(Assets.splash.fileName);
+		Assets.manager.finishLoadingAsset(Assets.skin.fileName);
+		loading = new Loading(this.game);
 	}
 
 	@Override
 	public void render(float delta) {
-		
+		exitButton();
+
+		// Clear the screen
+		Gdx.gl.glClearColor(1f, 1f, 1f, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		//Update the stage
+    	loading.act(delta);
+    	loading.draw();
+    	
+		if (Assets.manager.update()) {
+			// create rest of screens
+			game.screens.put("Main Menu", new MainMenuScreen(game));	//only one of
+			game.screens.put("Settings Menu", new SettingsMenuScreen(game));	//only one of
+			//game.screens.put("Play", new PlayScreen(game));	//new one may be created
+			game.setScreen(game.screens.get("Main Menu"));
+		}
 	}
 
 	@Override
 	public void resize(int width, int height) {
-		
+		loading.getViewport().update(width, height, true);
 	}
 
 	public void exitButton() {
-		
+		if (Gdx.input.isKeyPressed(Input.Keys.ESCAPE)) {
+			Gdx.app.exit();
+		}
 	}
 
 	@Override
 	public void dispose() {
-		
+		loading.dispose();
 	}
 
 	@Override
