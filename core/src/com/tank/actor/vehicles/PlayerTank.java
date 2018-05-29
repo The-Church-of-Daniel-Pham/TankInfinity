cbb56fa7e495f650a34b8da4d58b2fe14e452a3c
SamuelHunter <38480476+SamuelHunter@users.noreply.github.com>
2018-05-29 11:38:39 -0700

    removed reset method for playertank

--- core/src/com/tank/actor/vehicles/PlayerTank.java
+++ core/src/com/tank/actor/vehicles/PlayerTank.java
@@ -9,17 +9,17 @@ import com.badlogic.gdx.math.Vector3;
 import com.tank.actor.projectiles.Bullet;
 import com.tank.actor.ui.Cursor;
 import com.tank.controls.ControlConstants;
-import com.tank.controls.KeyboardMouseController;
 import com.tank.controls.TankController;
 import com.tank.stats.Customization;
 import com.tank.subweapons.SubWeapon;
+import com.tank.stage.Level;
 
 public class PlayerTank extends FreeTank implements InputProcessor {
 	/**
 	 * used for spawning bullets the correct distance away from Vehicle's center
 	 */
-	public static final int TANK_GUN_LENGTH = 135;	//probably redundant, check free tank
-	
+	public static final int TANK_GUN_LENGTH = 135; // probably redundant, check free tank
+
 	protected TankController controls;
 	protected Cursor cursor;
 	protected Vector3 cursorPos;
@@ -31,9 +31,9 @@ public class PlayerTank extends FreeTank implements InputProcessor {
 	protected Customization custom;
 
 	protected float reloadTime;
-	
+
 	public PlayerTank(int playerNumber) {
-		super(0, 0);	// defaults
+		super(0, 0); // defaults
 		initializeCustom("default", "default");
 		this.playerNumber = playerNumber;
 		initializeStats();
@@ -59,7 +59,7 @@ public class PlayerTank extends FreeTank implements InputProcessor {
 		super.setGunOffsetX(-12);
 		super.setGunPivotX(treadTexture.getWidth() / 2 - super.getGunOffsetX());
 	}
-	
+
 	protected void initializeStats() {
 		stats.addStat("Friction", 95); // (fraction out of 100)^delta to scale velocity by
 		stats.addStat("Acceleration", 1200);
@@ -67,7 +67,7 @@ public class PlayerTank extends FreeTank implements InputProcessor {
 		stats.addStat("Angular_Acceleration", 300);
 		stats.addStat("Rate_Of_Fire", 1);
 	}
-	
+
 	protected void initializeCustom(String tColor, String gColor) {
 		custom = new Customization();
 		custom.setCustom("tread", tColor);
@@ -77,14 +77,10 @@ public class PlayerTank extends FreeTank implements InputProcessor {
 		this.tColor = tColor;
 		this.gColor = gColor;
 	}
-	
-	public void reset() {
-		reloadTime = 0;
-		selectedWeapon = 0;
-		super.velocity = new Vector2(0, 0);
-		super.angularVelocity = 0;
-		super.setRotation(90);
-		super.gunRotation = 90;
+
+	private void setMapPosition(int row, int col) {
+		int[] newPos = ((Level) getStage()).getMap().getTileAt(row, col);
+		super.setPosition(newPos[0], newPos[1]);
 	}
 
 	/**
@@ -103,22 +99,21 @@ public class PlayerTank extends FreeTank implements InputProcessor {
 		}
 		super.applyFriction(delta);
 		super.move(delta);
-		//Gun Pointing
+		// Gun Pointing
 		if ((cursorPos = controls.getCursor(cursorPos)) != null) {
 			Vector3 cursorPos = getStage().getCamera().unproject(this.cursorPos.cpy()); // to world coordinates
 			cursor.setPosition(cursorPos.x, cursorPos.y);
 			super.pointGunToPoint(cursorPos.x, cursorPos.y);
 		}
-		//Firing
-		if (controls.firePressed() && reloadTime < 0.01) {	//if almsot done reloading, allow for rounding
+		// Firing
+		if (controls.firePressed() && reloadTime < 0.01) { // if almsot done reloading, allow for rounding
 			reloadTime = 1.0f / stats.getStatValue("Rate_Of_Fire");
 			shoot();
-		}
-		else if (reloadTime > 0){
+		} else if (reloadTime > 0) {
 			reloadTime -= delta;
 		}
 	}
-	
+
 	@Override
 	public void draw(Batch batch, float a) {
 		super.draw(batch, a);
@@ -149,7 +144,7 @@ public class PlayerTank extends FreeTank implements InputProcessor {
 		return new Polygon(f);
 
 	}
-	
+
 	/**
 	 * Set Vehicle customization unique to each player tank type
 	 */
