783c50b24ee6c7ce1794a37fb3e7fe754a08ab06
Samuel <Samuel@KIDS-PC>
2018-05-28 22:19:51 -0700

    fixed trigger and other controller button mappings

--- core/src/com/tank/controls/GamepadController.java
+++ core/src/com/tank/controls/GamepadController.java
@@ -1,133 +1,67 @@
 package com.tank.controls;
 
 import java.util.LinkedHashMap;
-
 import com.badlogic.gdx.controllers.Controller;
-import com.badlogic.gdx.controllers.ControllerListener;
 import com.badlogic.gdx.controllers.Controllers;
 import com.badlogic.gdx.controllers.PovDirection;
-import com.badlogic.gdx.controllers.mappings.Xbox;
 import com.badlogic.gdx.math.Vector3;
 
 public class GamepadController extends TankController {
 	public Controller controller;
+	private float tolerance = 0.75f;
 	private LinkedHashMap<String, Integer> keyMap;
 
 	public GamepadController() {
 		keyMap = new LinkedHashMap<String, Integer>();
 		keyMap.putAll(ControlConstants.DEFAULT_GAMEPAD_CONTROLS);
 		controller = Controllers.getControllers().first();
-		
-//		Controllers.addListener(new ControllerListener() {
-//			public int indexOf(Controller controller) {
-//				return Controllers.getControllers().indexOf(controller, true);
-//			}
-//
-//			@Override
-//			public void connected(Controller controller) {
-//				System.out.println("connected " + controller.getName());
-//				int i = 0;
-//				for (Controller c : Controllers.getControllers()) {
-//					System.out.println("#" + i++ + ": " + c.getName());
-//				}
-//			}
-//
-//			@Override
-//			public void disconnected(Controller controller) {
-//				System.out.println("disconnected " + controller.getName());
-//				int i = 0;
-//				for (Controller c : Controllers.getControllers()) {
-//					System.out.println("#" + i++ + ": " + c.getName());
-//				}
-//				if (Controllers.getControllers().size == 0)
-//					System.out.println("No controllers attached");
-//			}
-//
-//			@Override
-//			public boolean buttonDown(Controller controller, int buttonIndex) {
-//				System.out.println("#" + indexOf(controller) + ", button " + buttonIndex + " down");
-//				return false;
-//			}
-//
-//			@Override
-//			public boolean buttonUp(Controller controller, int buttonIndex) {
-//				System.out.println("#" + indexOf(controller) + ", button " + buttonIndex + " up");
-//				return false;
-//			}
-//
-//			@Override
-//			public boolean axisMoved(Controller controller, int axisIndex, float value) {
-//				System.out.println("#" + indexOf(controller) + ", axis " + axisIndex + ": " + value);
-//				return false;
-//			}
-//
-//			@Override
-//			public boolean povMoved(Controller controller, int povIndex, PovDirection value) {
-//				System.out.println("#" + indexOf(controller) + ", pov " + povIndex + ": " + value);
-//				return false;
-//			}
-//
-//			@Override
-//			public boolean xSliderMoved(Controller controller, int sliderIndex, boolean value) {
-//				System.out.println("#" + indexOf(controller) + ", x slider " + sliderIndex + ": " + value);
-//				return false;
-//			}
-//
-//			@Override
-//			public boolean ySliderMoved(Controller controller, int sliderIndex, boolean value) {
-//				System.out.println("#" + indexOf(controller) + ", y slider " + sliderIndex + ": " + value);
-//				return false;
-//			}
-//
-//			@Override
-//			public boolean accelerometerMoved(Controller controller, int accelerometerIndex, Vector3 value) {
-//				// not printing this as we get to many values
-//				return false;
-//			}
-//		});
+	}
+
+	public void setTolerance(float tol) {
+		tolerance = tol;
 	}
 
 	public boolean upPressed() {
 		if (controller == null) {
 			return false;
 		}
-		return controller.getAxis(keyMap.get("UP")) == -1;
+		return controller.getAxis(keyMap.get("UP")) < -tolerance;
 	}
 
 	public boolean downPressed() {
 		if (controller == null) {
 			return false;
 		}
-		return controller.getAxis(keyMap.get("DOWN")) == 1;
+		return controller.getAxis(keyMap.get("DOWN")) > tolerance;
 	}
 
 	public boolean rightPressed() {
 		if (controller == null) {
 			return false;
 		}
-		return controller.getAxis(keyMap.get("RIGHT")) == 1;
+		return controller.getAxis(keyMap.get("RIGHT")) > tolerance;
 	}
 
 	public boolean leftPressed() {
 		if (controller == null) {
 			return false;
 		}
-		return controller.getAxis(keyMap.get("LEFT")) == -1;
+		return controller.getAxis(keyMap.get("LEFT")) < -tolerance;
 	}
 
 	public boolean firePressed() {
+		// testButtons();
 		if (controller == null) {
 			return false;
 		}
-		testButtons();
-		return controller.getAxis(keyMap.get("SHOOT")) == -1;
+		return controller.getAxis(keyMap.get("SHOOT")) < -tolerance;
 	}
 
 	public boolean subPressed() {
 		if (controller == null) {
 			return false;
 		}
-		return controller.getAxis(keyMap.get("SUB")) == 1;
+		return controller.getAxis(keyMap.get("SUB")) > tolerance;
 	}
 
 	public boolean subRightPressed() {
@@ -143,16 +77,19 @@ public class GamepadController extends TankController {
 		}
 		return controller.getButton(keyMap.get("LSWITCH"));
 	}
-	
+
 	public void testButtons() {
 		for (int i = 0; i < 100; i++) {
-			if (controller.getButton(i)) System.out.println("Pressed Button " + i);
+			if (controller.getButton(i))
+				System.out.println("Pressed Button " + i);
 		}
 		for (int i = 0; i < 100; i++) {
-			if (Math.abs(controller.getAxis(i)) > 0.3) System.out.println("Axis " + i + " moved: " + controller.getAxis(i));
+			if (Math.abs(controller.getAxis(i)) > 0.3)
+				System.out.println("Axis " + i + " moved: " + controller.getAxis(i));
 		}
-		for (int i = 0; i< 100; i++) {
-			if (!controller.getPov(i).equals(PovDirection.center)) System.out.println("POV " + i + ": " + controller.getPov(i).toString());
+		for (int i = 0; i < 100; i++) {
+			if (!controller.getPov(i).equals(PovDirection.center))
+				System.out.println("POV " + i + ": " + controller.getPov(i).toString());
 		}
 	}
 
@@ -160,7 +97,6 @@ public class GamepadController extends TankController {
 		if (controller == null) {
 			return null;
 		}
-		return new Vector3(controller.getAxis(Xbox.R_STICK_HORIZONTAL_AXIS),
-				controller.getAxis(Xbox.R_STICK_VERTICAL_AXIS), 0);
+		return new Vector3(controller.getAxis(keyMap.get("XPOINT")), controller.getAxis(keyMap.get("YPOINT")), 0);
 	}
 }
