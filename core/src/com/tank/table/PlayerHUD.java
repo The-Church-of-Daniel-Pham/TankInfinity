74fd653d4a2894410a3e2c4339b638df91e5fcbf
Daniel <Daniel@DESKTOP-ND8SLK5>
2018-06-01 15:56:33 -0700

    Added image changing for subweapons

--- core/src/com/tank/table/PlayerHUD.java
+++ core/src/com/tank/table/PlayerHUD.java
@@ -1,12 +1,15 @@
 package com.tank.table;
 
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.ProgressBar;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.Align;
 import com.tank.game.Player;
+import com.tank.subweapons.SubWeapon;
 import com.tank.utils.Assets;
 
 public class PlayerHUD extends Table{
@@ -14,6 +17,11 @@ public class PlayerHUD extends Table{
 	protected ProgressBar reloadBar;
 	protected ProgressBar healthBar;
 	private Skin skin = Assets.manager.get(Assets.skin);
+	private final Image leftSubImage = new Image(Assets.manager.get(Assets.sub_empty));
+	private final Image centerSubImage = new Image(Assets.manager.get(Assets.sub_empty));
+	private final Image rightSubImage = new Image(Assets.manager.get(Assets.sub_empty));
+	private static TextureRegionDrawable subEmptyTex = new TextureRegionDrawable(new TextureRegion(Assets.manager.get(Assets.sub_empty)));
+	private SubWeapon lastCenterSubWeapon;
 	
 	public PlayerHUD(Player player) {
 		this.player = player;
@@ -24,9 +32,9 @@ public class PlayerHUD extends Table{
 		
 		Label nameLabel = new Label(player.getName(), skin);
 		nameLabel.setAlignment(Align.left);
-		final Image leftSubImage = new Image(Assets.manager.get(Assets.sub_empty));
-		final Image centerSubImage = new Image(Assets.manager.get(Assets.sub_empty));
-		final Image rightSubImage = new Image(Assets.manager.get(Assets.sub_empty));
+		//final Image leftSubImage = new Image(Assets.manager.get(Assets.sub_empty));
+		//final Image centerSubImage = new Image(Assets.manager.get(Assets.sub_empty));
+		//final Image rightSubImage = new Image(Assets.manager.get(Assets.sub_empty));
 		reloadBar = new ProgressBar(0.0f, 1.0f, 0.01f, false, skin);
 		healthBar = new ProgressBar(0.0f, player.tank.getMaxHealth(), 0.01f, false, skin);
 
@@ -42,10 +50,30 @@ public class PlayerHUD extends Table{
 	}
 	
 	public void update() {
+		if (subChanged()) {
+			if (player.tank.getCurrentSubWeapon() != null) {
+				centerSubImage.setDrawable(new TextureRegionDrawable(new TextureRegion(player.tank.getCurrentSubWeapon().getTexture())));
+				leftSubImage.setDrawable(new TextureRegionDrawable(new TextureRegion(player.tank.getPrevSubWeapon().getTexture())));
+				rightSubImage.setDrawable(new TextureRegionDrawable(new TextureRegion(player.tank.getNextSubWeapon().getTexture())));
+			}
+			else {
+				centerSubImage.setDrawable(subEmptyTex);
+				leftSubImage.setDrawable(subEmptyTex);
+				rightSubImage.setDrawable(subEmptyTex);
+			}
+			lastCenterSubWeapon = player.tank.getCurrentSubWeapon();
+		}
 		float completion = player.tank.getReloadTime() / player.tank.getLastReloadTime();;
 		// reload time out of max reload time (inverse of rate of fire)
 		//System.out.println(completion);
 		reloadBar.setValue(completion);
 		healthBar.setValue(player.tank.getHealth());
+	}
+	
+	public boolean subChanged() {
+		if (player.tank.getCurrentSubWeapon() == null && lastCenterSubWeapon == null) return false;
+		if (player.tank.getCurrentSubWeapon() != null && lastCenterSubWeapon == null) return true;
+		if (player.tank.getCurrentSubWeapon() == null && lastCenterSubWeapon != null) return true;
+		return !player.tank.getCurrentSubWeapon().equals(lastCenterSubWeapon);
 	}
 }
