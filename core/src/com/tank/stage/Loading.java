319c97e8af65d2b2396406d4887954beb9a3df5e
SamuelHunter <38480476+SamuelHunter@users.noreply.github.com>
2018-05-31 17:59:13 -0700

    fixed tips on loading, added main menu background - both 4k

--- core/src/com/tank/stage/Loading.java
+++ core/src/com/tank/stage/Loading.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Interpolation;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
@@ -15,13 +16,14 @@ import com.tank.utils.Assets;
 
 public class Loading extends Stage implements InputProcessor {
 	protected TankInfinity game;
+	protected Table uiTable;
 	private Background tankLoadingBackground;
 	private float distance;
 	private float percent;
 	
-	private Skin skin = Assets.manager.get(Assets.skin);
-	private Texture backdrop = Assets.manager.get(Assets.backdrop);
-	private Texture loading_tank = Assets.manager.get(Assets.loading_tank);
+	protected Skin skin = Assets.manager.get(Assets.skin);
+	protected Texture backdrop = Assets.manager.get(Assets.backdrop);
+	protected Texture loading_tank = Assets.manager.get(Assets.loading_tank);
 	
 	public Loading(TankInfinity game) {
 		super(new ExtendViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight()));
@@ -32,9 +34,10 @@ public class Loading extends Stage implements InputProcessor {
 		tankLoadingBackground.setPosition(-loading_tank.getWidth(), 300);
 		percent = 0;
 		distance = Gdx.graphics.getWidth() + loading_tank.getWidth();
+		buildTable();
 		super.addActor(backdropBackground);
 		super.addActor(tankLoadingBackground);
-		super.addActor(buildTable());
+		super.addActor(uiTable);
 	}
 	
 	@Override
@@ -43,17 +46,14 @@ public class Loading extends Stage implements InputProcessor {
 		tankLoadingBackground.setX(percent * distance);
 	}
 
-	private Table buildTable() {
-		Table uiTable = new Table();
-		uiTable.setFillParent(false);
-		uiTable.setDebug(true);
-		uiTable.bottom().padBottom(200).right().padRight(100);
-		uiTable.defaults().width(400).height(200).space(25).right();
+	private void buildTable() {
+		uiTable = new Table();
+		uiTable.setFillParent(true);
+		uiTable.setDebug(false);
+		uiTable.bottom().padBottom(100).right().padRight(50);
 
 		// Add widgets to the table here.
-		Label tipLabel = new Label("Get gud", skin);
-		uiTable.add(tipLabel);
-
-		return uiTable;
+		Label tipLabel = new Label("Git gud", skin);
+		uiTable.add(tipLabel).width(500).height(100).right();
 	}
 }
