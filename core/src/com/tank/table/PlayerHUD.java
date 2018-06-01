d6b234b5415bb9d7a4cbeec0ac8c386931de33cd
SamuelHunter <38480476+SamuelHunter@users.noreply.github.com>
2018-06-01 11:33:10 -0700

    secondaries menu start

--- core/src/com/tank/table/PlayerHUD.java
+++ core/src/com/tank/table/PlayerHUD.java
@@ -1,5 +1,6 @@
 package com.tank.table;
 
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.ProgressBar;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
@@ -17,19 +18,27 @@ public class PlayerHUD extends Table{
 	public PlayerHUD(Player player) {
 		this.player = player;
 		
+		super.setFillParent(false);
 		super.setDebug(false);
-		super.defaults().width(300).height(50).space(25).center();
+		super.defaults().width(150).height(25).space(25).center();
 		
 		Label nameLabel = new Label(player.getName(), skin);
 		nameLabel.setAlignment(Align.left);
+		final Image leftSubImage = new Image(Assets.manager.get(Assets.sub_empty));
+		final Image centerSubImage = new Image(Assets.manager.get(Assets.sub_empty));
+		final Image rightSubImage = new Image(Assets.manager.get(Assets.sub_empty));
 		reloadBar = new ProgressBar(0.0f, 1.0f, 0.01f, false, skin);
 		healthBar = new ProgressBar(0.0f, player.tank.getMaxHealth(), 0.01f, false, skin);
 
-		super.add(nameLabel).left();
+		super.add(nameLabel).left().colspan(3);
 		super.row();
-		super.add(reloadBar).width(300);
+		super.add(leftSubImage).width(50).height(50);
+		super.add(centerSubImage).width(50).height(50);
+		super.add(rightSubImage).width(50).height(50);
 		super.row();
-		super.add(healthBar).width(300);
+		super.add(reloadBar).colspan(3);
+		super.row();
+		super.add(healthBar).colspan(3);
 	}
 	
 	public void update() {
