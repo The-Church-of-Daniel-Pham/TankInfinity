bbabf6225d8e85831c70fb84e11e3a6f783a270a
Samuel <Samuel@KIDS-PC>
2018-06-02 13:07:32 -0700

    border tile textures

--- core/src/com/tank/actor/map/tiles/BorderTile.java
+++ core/src/com/tank/actor/map/tiles/BorderTile.java
@@ -11,5 +11,15 @@ public class BorderTile extends WallTile{
 	@Override
 	public void build() {
 		super.addTexture(Assets.manager.get(Assets.grass0));
+		double rand = Math.random();
+		if (rand < 0.25) {
+			super.addTexture(Assets.manager.get(Assets.border1));
+		} else if (rand < 0.5) {
+			super.addTexture(Assets.manager.get(Assets.border2));
+		} else if (rand < 0.75) {
+			super.addTexture(Assets.manager.get(Assets.border3));
+		} else {
+			super.addTexture(Assets.manager.get(Assets.border4));
+		}
 	}
 }
