f4550ca07b19efa0d4f5a627f0c698e9f724f947
SamuelHunter <38480476+SamuelHunter@users.noreply.github.com>
2018-06-03 14:39:43 -0700

    retroactively added 6-2 build

--- core/src/com/tank/actor/projectiles/Laser.java
+++ core/src/com/tank/actor/projectiles/Laser.java
@@ -1,9 +1,7 @@
 package com.tank.actor.projectiles;
 
 import java.util.ArrayList;
-
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.math.Polygon;
 import com.badlogic.gdx.math.Vector2;
 import com.tank.actor.map.tiles.AbstractMapTile;
