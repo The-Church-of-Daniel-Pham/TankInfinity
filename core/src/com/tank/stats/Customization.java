9781b82a7228bb612fc40039cfc843f8b30521c3
Samuel <Samuel@KIDS-PC>
2018-06-03 14:15:10 -0700

    new artillery markers with matching colors

--- core/src/com/tank/stats/Customization.java
+++ core/src/com/tank/stats/Customization.java
@@ -1,6 +1,7 @@
 package com.tank.stats;
 
 import java.util.LinkedHashMap;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.tank.utils.Assets;
 import com.tank.utils.CycleList;
@@ -11,7 +12,7 @@ public class Customization {
 
 	public Customization() {
 		custom = new LinkedHashMap<String, String>();
-		colors = new CycleList<String> (new String[] {"red", "blue", "green", "yellow"}, 0, true);
+		colors = new CycleList<String>(new String[] { "red", "blue", "green", "yellow" }, 0, true);
 	}
 
 	public void setCustom(String cust, String val) {
@@ -29,11 +30,31 @@ public class Customization {
 	public boolean hasCustom(String cust) {
 		return (custom.containsKey(cust));
 	}
-	
+
 	public void cycleCustom(String cust, int n) {
 		if (cust.equals("tank color")) {
 			colors.cycleBy(n);
 			custom.put(cust, (String) colors.getCurrent());
+		}
+	}
+
+	public Color getColor() {
+		// from colors.psd
+		String val = getCustomValue("tank color");
+		if (val.equals("red")) {
+			return new Color(204f / 255f, 4f / 255f, 4f / 255f, 1.0f);
+		}
+		else if (val.equals("blue")) {
+			return new Color(4f / 255f, 112f / 255f, 204f / 255f, 1.0f);
+		}
+		else if (val.equals("green")) {
+			return new Color(4f / 255f, 204f / 255f, 93f / 255f, 1.0f);
+		}
+		else if (val.equals("yellow")) {
+			return new Color(204f / 255f, 197f / 255f, 4f / 255f, 1.0f);
+		}
+		else {
+			return new Color(0f / 255f, 0f / 255f, 0f / 255f, 1.0f);
 		}
 	}
 
@@ -42,71 +63,64 @@ public class Customization {
 		if (name.equals("tread")) {
 			if (val.equals("red")) {
 				return Assets.manager.get(Assets.tread_red);
-			}
-			else if (val.equals("blue")) {
+			} else if (val.equals("blue")) {
 				return Assets.manager.get(Assets.tread_blue);
-			}
-			else if (val.equals("green")) {
+			} else if (val.equals("green")) {
 				return Assets.manager.get(Assets.tread_green);
-			}
-			else if (val.equals("yellow")) {
+			} else if (val.equals("yellow")) {
 				return Assets.manager.get(Assets.tread_yellow);
-			}
-			else {
+			} else {
 				return Assets.manager.get(Assets.tread_red);
 			}
-		}
-		else if (name.equals("gun")) {
+		} else if (name.equals("gun")) {
 			if (val.equals("red")) {
 				return Assets.manager.get(Assets.gun_red);
-			}
-			else if (val.equals("blue")) {
+			} else if (val.equals("blue")) {
 				return Assets.manager.get(Assets.gun_blue);
-			}
-			else if (val.equals("green")) {
+			} else if (val.equals("green")) {
 				return Assets.manager.get(Assets.gun_green);
-			}
-			else if (val.equals("yellow")) {
+			} else if (val.equals("yellow")) {
 				return Assets.manager.get(Assets.gun_yellow);
-			}
-			else {
+			} else {
 				return Assets.manager.get(Assets.gun_red);
 			}
-		}
-		else if (name.equals("cursor")) {
+		} else if (name.equals("cursor")) {
 			if (val.equals("red")) {
 				return Assets.manager.get(Assets.crosshairs_red);
-			}
-			else if (val.equals("blue")) {
+			} else if (val.equals("blue")) {
 				return Assets.manager.get(Assets.crosshairs_blue);
-			}
-			else if (val.equals("green")) {
+			} else if (val.equals("green")) {
 				return Assets.manager.get(Assets.crosshairs_green);
-			}
-			else if (val.equals("yellow")) {
+			} else if (val.equals("yellow")) {
 				return Assets.manager.get(Assets.crosshairs_yellow);
+			} else {
+				return Assets.manager.get(Assets.crosshairs_black);
 			}
-			else {
-				return Assets.manager.get(Assets.crosshairs_default);
+		} else if (name.equals("artillery")) {
+			if (val.equals("red")) {
+				return Assets.manager.get(Assets.artillery_crosshairs_red);
+			} else if (val.equals("blue")) {
+				return Assets.manager.get(Assets.artillery_crosshairs_blue);
+			} else if (val.equals("green")) {
+				return Assets.manager.get(Assets.artillery_crosshairs_green);
+			} else if (val.equals("yellow")) {
+				return Assets.manager.get(Assets.artillery_crosshairs_yellow);
+			} else {
+				return Assets.manager.get(Assets.artillery_crosshairs_black);
 			}
-		}
-		else if (name.equals("preview")) {
+		} else if (name.equals("preview")) {
 			if (val.equals("red")) {
 				return Assets.manager.get(Assets.tank_preview_red);
-			}
-			else if (val.equals("blue")) {
+			} else if (val.equals("blue")) {
 				return Assets.manager.get(Assets.tank_preview_blue);
-			}
-			else if (val.equals("green")) {
+			} else if (val.equals("green")) {
 				return Assets.manager.get(Assets.tank_preview_green);
-			}
-			else if (val.equals("yellow")) {
+			} else if (val.equals("yellow")) {
 				return Assets.manager.get(Assets.tank_preview_yellow);
-			}
-			else {
+			} else {
 				return Assets.manager.get(Assets.tank_preview_red);
 			}
 		}
-		return null;	//not found
+		return null; // not found
 	}
 }
