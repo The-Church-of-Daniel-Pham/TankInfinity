9e1289f2f28b517c558a7dd6279cb5a76083d590
Daniel <Daniel@DESKTOP-ND8SLK5>
2018-06-03 15:50:27 -0700

    Background scales properly

--- core/src/com/tank/actor/ui/Background.java
+++ core/src/com/tank/actor/ui/Background.java
@@ -1,6 +1,5 @@
 package com.tank.actor.ui;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.scenes.scene2d.Actor;
