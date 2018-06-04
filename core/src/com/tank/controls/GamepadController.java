f624815771228da68605063bdb04f7d6fe6a42e8
Android <Android@00-67077.pusd.dom>
2018-06-04 12:14:28 -0700

    Fixed gamepad controller to get controller by index

--- core/src/com/tank/controls/GamepadController.java
+++ core/src/com/tank/controls/GamepadController.java
@@ -6,25 +6,28 @@ import com.badlogic.gdx.controllers.Controller;
 import com.badlogic.gdx.controllers.Controllers;
 import com.badlogic.gdx.controllers.PovDirection;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.Array;
 
 public class GamepadController extends TankController {
-	public Controller controller;
+	public int index;
 	private LinkedHashMap<String, KeyControl> keyMap;
 	private float deadzone = 0.30f;
 	private float sensitivity = 15f;
-	private static ArrayList<Controller> inUse = new ArrayList<Controller>();
+	private static ArrayList<Integer> inUse = new ArrayList<Integer>();
+	private static Array<Controller> controllers = Controllers.getControllers();
 
 	public GamepadController() throws Exception {
 		keyMap = new LinkedHashMap<String, KeyControl>();
 		keyMap.putAll(ControlConstants.DEFAULT_GAMEPAD_CONTROLS);
-		for (Controller controller : Controllers.getControllers()) {
-			if (!inUse.contains(controller)) {
-				this.controller = controller;
-				inUse.add(controller);
+		index = -1;
+		for (int i = 0; i < controllers.size; i++) {
+			if (!inUse.contains(index)) {
+				this.index = i;
+				inUse.add(index);
 				break;
 			}
 		}
-		if (controller == null) throw new Exception("No more controllers");
+		if (index == -1) throw new Exception("No more controllers");
 	}
 
 	public void setDeadzone(float d) {
@@ -36,19 +39,19 @@ public class GamepadController extends TankController {
 	}
 
 	public boolean upPressed() {
-		if (controller == null) {
+		if (controllers.get(index) == null) {
 			return false;
 		}
 		KeyControl up = keyMap.get("UP");
 		if (up.getKeyType() == 0) {
-			return controller.getButton(up.getKeyCode());
+			return controllers.get(index).getButton(up.getKeyCode());
 		}
 		else {
 			if (up.getDirection() < 0) {
-				return (controller.getAxis(up.getKeyCode()) <= -deadzone);
+				return (controllers.get(index).getAxis(up.getKeyCode()) <= -deadzone);
 			}
 			else if (up.getDirection() > 0) {
-				return (controller.getAxis(up.getKeyCode()) >= deadzone);
+				return (controllers.get(index).getAxis(up.getKeyCode()) >= deadzone);
 			}
 				
 		}
@@ -56,19 +59,19 @@ public class GamepadController extends TankController {
 	}
 
 	public boolean downPressed() {
-		if (controller == null) {
+		if (controllers.get(index) == null) {
 			return false;
 		}
 		KeyControl down = keyMap.get("DOWN");
 		if (down.getKeyType() == 0) {
-			return controller.getButton(down.getKeyCode());
+			return controllers.get(index).getButton(down.getKeyCode());
 		}
 		else {
 			if (down.getDirection() < 0) {
-				return (controller.getAxis(down.getKeyCode()) <= -deadzone);
+				return (controllers.get(index).getAxis(down.getKeyCode()) <= -deadzone);
 			}
 			else if (down.getDirection() > 0) {
-				return (controller.getAxis(down.getKeyCode()) >= deadzone);
+				return (controllers.get(index).getAxis(down.getKeyCode()) >= deadzone);
 			}
 				
 		}
@@ -76,19 +79,19 @@ public class GamepadController extends TankController {
 	}
 
 	public boolean rightPressed() {
-		if (controller == null) {
+		if (controllers.get(index) == null) {
 			return false;
 		}
 		KeyControl right = keyMap.get("RIGHT");
 		if (right.getKeyType() == 0) {
-			return controller.getButton(right.getKeyCode());
+			return controllers.get(index).getButton(right.getKeyCode());
 		}
 		else {
 			if (right.getDirection() < 0) {
-				return (controller.getAxis(right.getKeyCode()) <= -deadzone);
+				return (controllers.get(index).getAxis(right.getKeyCode()) <= -deadzone);
 			}
 			else if (right.getDirection() > 0) {
-				return (controller.getAxis(right.getKeyCode()) >= deadzone);
+				return (controllers.get(index).getAxis(right.getKeyCode()) >= deadzone);
 			}
 				
 		}
@@ -96,19 +99,19 @@ public class GamepadController extends TankController {
 	}
 
 	public boolean leftPressed() {
-		if (controller == null) {
+		if (controllers.get(index) == null) {
 			return false;
 		}
 		KeyControl left = keyMap.get("LEFT");
 		if (left.getKeyType() == 0) {
-			return controller.getButton(left.getKeyCode());
+			return controllers.get(index).getButton(left.getKeyCode());
 		}
 		else {
 			if (left.getDirection() < 0) {
-				return (controller.getAxis(left.getKeyCode()) <= -deadzone);
+				return (controllers.get(index).getAxis(left.getKeyCode()) <= -deadzone);
 			}
 			else if (left.getDirection() > 0) {
-				return (controller.getAxis(left.getKeyCode()) >= deadzone);
+				return (controllers.get(index).getAxis(left.getKeyCode()) >= deadzone);
 			}
 				
 		}
@@ -117,19 +120,19 @@ public class GamepadController extends TankController {
 
 	public boolean firePressed() {
 		//testButtons();
-		if (controller == null) {
+		if (controllers.get(index) == null) {
 			return false;
 		}
 		KeyControl shoot = keyMap.get("SHOOT");
 		if (shoot.getKeyType() == 0) {
-			return controller.getButton(shoot.getKeyCode());
+			return controllers.get(index).getButton(shoot.getKeyCode());
 		}
 		else {
 			if (shoot.getDirection() < 0) {
-				return (controller.getAxis(shoot.getKeyCode()) <= -deadzone);
+				return (controllers.get(index).getAxis(shoot.getKeyCode()) <= -deadzone);
 			}
 			else if (shoot.getDirection() > 0) {
-				return (controller.getAxis(shoot.getKeyCode()) >= deadzone);
+				return (controllers.get(index).getAxis(shoot.getKeyCode()) >= deadzone);
 			}
 				
 		}
@@ -137,19 +140,19 @@ public class GamepadController extends TankController {
 	}
 
 	public boolean subPressed() {
-		if (controller == null) {
+		if (controllers.get(index) == null) {
 			return false;
 		}
 		KeyControl sub = keyMap.get("SUB");
 		if (sub.getKeyType() == 0) {
-			return controller.getButton(sub.getKeyCode());
+			return controllers.get(index).getButton(sub.getKeyCode());
 		}
 		else {
 			if (sub.getDirection() < 0) {
-				return (controller.getAxis(sub.getKeyCode()) <= -deadzone);
+				return (controllers.get(index).getAxis(sub.getKeyCode()) <= -deadzone);
 			}
 			else if (sub.getDirection() > 0) {
-				return (controller.getAxis(sub.getKeyCode()) >= deadzone);
+				return (controllers.get(index).getAxis(sub.getKeyCode()) >= deadzone);
 			}
 				
 		}
@@ -157,19 +160,19 @@ public class GamepadController extends TankController {
 	}
 
 	public boolean subRightPressed() {
-		if (controller == null) {
+		if (controllers.get(index) == null) {
 			return false;
 		}
 		KeyControl rSwitch = keyMap.get("RSHIFT");
 		if (rSwitch.getKeyType() == 0) {
-			return controller.getButton(rSwitch.getKeyCode());
+			return controllers.get(index).getButton(rSwitch.getKeyCode());
 		}
 		else {
 			if (rSwitch.getDirection() < 0) {
-				return (controller.getAxis(rSwitch.getKeyCode()) <= -deadzone);
+				return (controllers.get(index).getAxis(rSwitch.getKeyCode()) <= -deadzone);
 			}
 			else if (rSwitch.getDirection() > 0) {
-				return (controller.getAxis(rSwitch.getKeyCode()) >= deadzone);
+				return (controllers.get(index).getAxis(rSwitch.getKeyCode()) >= deadzone);
 			}
 				
 		}
@@ -177,19 +180,19 @@ public class GamepadController extends TankController {
 	}
 
 	public boolean subLeftPressed() {
-		if (controller == null) {
+		if (controllers.get(index) == null) {
 			return false;
 		}
 		KeyControl lSwitch = keyMap.get("LSHIFT");
 		if (lSwitch.getKeyType() == 0) {
-			return controller.getButton(lSwitch.getKeyCode());
+			return controllers.get(index).getButton(lSwitch.getKeyCode());
 		}
 		else {
 			if (lSwitch.getDirection() < 0) {
-				return (controller.getAxis(lSwitch.getKeyCode()) <= -deadzone);
+				return (controllers.get(index).getAxis(lSwitch.getKeyCode()) <= -deadzone);
 			}
 			else if (lSwitch.getDirection() > 0) {
-				return (controller.getAxis(lSwitch.getKeyCode()) >= deadzone);
+				return (controllers.get(index).getAxis(lSwitch.getKeyCode()) >= deadzone);
 			}
 				
 		}
@@ -197,19 +200,19 @@ public class GamepadController extends TankController {
 	}
 	
 	public boolean pausePressed() {
-		if (controller == null) {
+		if (controllers.get(index) == null) {
 			return false;
 		}
 		KeyControl pause = keyMap.get("PAUSE");
 		if (pause.getKeyType() == 0) {
-			return controller.getButton(pause.getKeyCode());
+			return controllers.get(index).getButton(pause.getKeyCode());
 		}
 		else {
 			if (pause.getDirection() < 0) {
-				return (controller.getAxis(pause.getKeyCode()) <= -deadzone);
+				return (controllers.get(index).getAxis(pause.getKeyCode()) <= -deadzone);
 			}
 			else if (pause.getDirection() > 0) {
-				return (controller.getAxis(pause.getKeyCode()) >= deadzone);
+				return (controllers.get(index).getAxis(pause.getKeyCode()) >= deadzone);
 			}
 				
 		}
@@ -218,28 +221,28 @@ public class GamepadController extends TankController {
 
 	public void testButtons() {
 		for (int i = 0; i < 100; i++) {
-			if (controller.getButton(i))
+			if (controllers.get(index).getButton(i))
 				System.out.println("Pressed Button " + i);
 		}
 		for (int i = 0; i < 100; i++) {
-			if (Math.abs(controller.getAxis(i)) > 0.3)
-				System.out.println("Axis " + i + " moved: " + controller.getAxis(i));
+			if (Math.abs(controllers.get(index).getAxis(i)) > 0.3)
+				System.out.println("Axis " + i + " moved: " + controllers.get(index).getAxis(i));
 		}
 		for (int i = 0; i < 100; i++) {
-			if (!controller.getPov(i).equals(PovDirection.center))
-				System.out.println("POV " + i + ": " + controller.getPov(i).toString());
+			if (!controllers.get(index).getPov(i).equals(PovDirection.center))
+				System.out.println("POV " + i + ": " + controllers.get(index).getPov(i).toString());
 		}
 	}
 
 	public Vector3 getCursor(Vector3 oldCursor) {
-		if (controller == null) 
+		if (controllers.get(index) == null) 
 			return null;
 		KeyControl vertical = keyMap.get("CURSOR-V");
 		KeyControl horizontal = keyMap.get("CURSOR-H");
 		float newX = oldCursor.x;
 		float newY = oldCursor.y;
-		if (Math.abs(controller.getAxis(horizontal.getKeyCode())) > deadzone) newX += sensitivity * controller.getAxis(horizontal.getKeyCode());
-		if (Math.abs(controller.getAxis(vertical.getKeyCode())) > deadzone) newY += sensitivity * controller.getAxis(vertical.getKeyCode());
+		if (Math.abs(controllers.get(index).getAxis(horizontal.getKeyCode())) > deadzone) newX += sensitivity * controllers.get(index).getAxis(horizontal.getKeyCode());
+		if (Math.abs(controllers.get(index).getAxis(vertical.getKeyCode())) > deadzone) newY += sensitivity * controllers.get(index).getAxis(vertical.getKeyCode());
 		return new Vector3(newX, newY, 0);
 	}
 	
